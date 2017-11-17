dd725e747b0daba1462468fa128e50ce3de0ae47
tejaram15 <raghuramkarra15@gmail.com>
2017-11-17 08:33:24 +0530

    Updated Dialouge Corpous, Data Filteration and Weights improved hdf5 files with ipynb checkpoints.

--- Keras Model.py
+++ Keras Model.py
@@ -47,4 +47,4 @@ filepath = "weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 # fit the model
-model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+model.fit(X, y, epochs=50, batch_size=512, callbacks=callbacks_list)
